a37fc62a0f02f53be84bd6fc14dfc92e7a89da64
Sean Wilson <seancwilson@gmail.com>
2021-01-31 12:09:35 -0600

    add piano keyboard component

---
+++ src/app.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import VPianoKeyboard from 'vue-piano-keyboard'
+
+@Component({
+	components: { VPianoKeyboard },
+})
+export default class App extends Vue {}
+</script>
+
+
+<template  lang="pug">
+
+	div#app
+		v-piano-keyboard#piano(show-keys)
+
+</template>
+
+
+<style lang="sass" scoped>
+
+	#app
+		font-family: Avenir, Helvetica, Arial, sans-serif
+		-webkit-font-smoothing: antialiased
+		-moz-osx-font-smoothing: grayscale
+		text-align: center
+		color: #2c3e50
+		margin-top: 60px
+
+		#piano
+			height: 300px
+
+</style>
