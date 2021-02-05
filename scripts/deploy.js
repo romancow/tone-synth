ca1b29ddead1b77d5af954bf7e195c0bc48727be
Sean Wilson <seancwilson@gmail.com>
2021-02-05 01:02:36 -0600

    update deploy script & favicon

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,26 @@
 const { version } = require('../package.json')
 const { exec } = require('child_process')
 
+const {
+	GIT_CONFIG_NAME,
+	GIT_CONFIG_EMAIL
+} = process.env
+
 const options = { cwd: 'dist' }
 const command = [
-	"git init",
+	"git init -b master",
+	GIT_CONFIG_NAME && `git config user.name "${GIT_CONFIG_NAME}"`,
+	GIT_CONFIG_EMAIL && `git config user.email "${GIT_CONFIG_EMAIL}"`,
 	"git add -A",
 	`git commit -m 'v${version}'`,
 	"git push -f git@github.com:romancow/tone-synth.git master:gh-pages"
-].join("\n")
+].filter(cmd => !!cmd).join(" && ")
 
 exec(command, options, (error, stdout, stderr) => {
 	if (stdout) console.log(`STDOUT: ${stdout}`)
 	if (stderr) console.error(`STDERR: ${stderr}`)
-	if (error) console.error(`ERROR: ${error}`)
+	if (error) {
+		console.error(`ERROR: ${error}`)
+		throw error
+	}
 })
