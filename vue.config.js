326f1a455dd426fad68ae871ecc6998f6ee9a365
Sean Wilson <seancwilson@gmail.com>
2021-02-05 15:13:19 -0600

    add pug index

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
+const package = require('./package.json')
 const { NODE_ENV } = process.env
 
 module.exports = {
 	publicPath: (NODE_ENV === 'production') ? '/tone-synth/' : '/',
 	pluginOptions: {
 		html: {
-			title: "Tone Synth"
+			entry: "src/main.ts",
+			template: "!!pug-loader!src/index.pug",
+			hash: true,
+			title: "Tone Synth",
+			version: package.version,
 		}
 	}
 }
